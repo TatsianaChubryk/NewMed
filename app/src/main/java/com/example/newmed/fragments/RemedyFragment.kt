396138f2e8a7d5e9985ca78125e19ec6e05adb60
TatsianaChubryk <84435613+TatsianaChubryk@users.noreply.github.com>
2021-10-31 12:05:25 +0300

    search on Google Map

--- app/src/main/java/com/example/newmed/fragments/RemedyFragment.kt
+++ app/src/main/java/com/example/newmed/fragments/RemedyFragment.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.newmed.PatientApplication
-import com.example.newmed.R
 import com.example.newmed.adapter.RemedyListAdapter
 import com.example.newmed.adapter.RemedyListener
-import com.example.newmed.database.RemedyEntity
 import com.example.newmed.databinding.FragmentRemedyListBinding
 import com.example.newmed.viewmodel.RemedyViewModel
 import com.example.newmed.reposotiry.RemedyViewModelFactory
@@ -48,18 +45,5 @@ class RemedyFragment : Fragment() {
         remedyViewModel.allRemedy.observe(viewLifecycleOwner) {
             adapter.submitList(it)
         }
-
-        binding.btnUpdateRemedy.setOnClickListener {
-            updateRemedy()
-        }
-    }
-
-    private fun updateRemedy() {
-        /*val remedy = binding.remedyRecyclerView.tvRemedy.text.toString()
-        val amountRemedy = binding.btnUpdateRemedy.etRemedyAmount.editText
-
-        val update = RemedyEntity(arguments?.getInt("patientId") ?: 0, remedy, amountRemedy)
-        remedyViewModel.updateRemedy(update)
-        Toast.makeText(requireContext(), getString(R.string.update_info), Toast.LENGTH_SHORT).show()*/
     }
 }
